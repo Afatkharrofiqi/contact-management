991018b2f84a88de20a0663406c2bcd8b89878ef
fatkharrofiqi <achmadfatkharrofiqi404@gmail.com>
2024-10-28 05:42:59 +0700

    feat: add login

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,12 @@
 import { HttpException, Inject, Injectable } from '@nestjs/common';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { ValidationService } from 'src/common/validation.service';
-import { RegisterUserRequest, UserResponse } from 'src/model/user.model';
+import { LoginUserRequest, RegisterUserRequest, UserResponse } from 'src/model/user.model';
 import { PrismaService } from 'src/common/prisma.service';
 import { Logger } from 'winston';
 import { UserValidation } from './user.validation';
 import * as bcrypt from 'bcrypt';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class UserService {
@@ -38,5 +39,41 @@ export class UserService {
             username: user.username,
             name: user.name,
         };
+    }
+
+    async login(request: LoginUserRequest): Promise<UserResponse> {
+        this.logger.info(`UserService.login(${JSON.stringify(request)})`);
+        const loginRequest: LoginUserRequest = this.validationService.validate(UserValidation.LOGIN, request);
+
+        let user = await this.prismaService.user.findUnique({
+            where: {
+                username: loginRequest.username
+            }
+        });
+
+        if (!user) {
+            throw new HttpException('Username or password is invalid', 401);
+        }
+
+        const isPasswordValid = await bcrypt.compare(loginRequest.password, user.password);
+
+        if (!isPasswordValid) {
+            throw new HttpException('Username or password is invalid', 401);
+        }
+
+        user = await this.prismaService.user.update({
+            where: {
+                username: request.username
+            },
+            data: {
+                token: uuid()
+            }
+        });
+
+        return {
+            username: user.username,
+            name: user.name,
+            token: user.token
+        }
     }
 }
